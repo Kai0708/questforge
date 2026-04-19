26beadd026c94335b98737579a503d388d15eb94
Kai0708 <kaiwenz0708@gmail.com>
2026-04-18 21:48:49 -0700

    Remove OAuth session handling from main.jsx

--- questforge/src/main.jsx
+++ questforge/src/main.jsx
@@ -3,15 +3,9 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
-import { supabase } from './lib/supabase.js'
-
-// Handle OAuth callback
-supabase.auth.getSession()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 )
